79dad0ec121d436eedfe2380329a758041034d45
LeThiThuyNgan <Lethithuyngan3032004@gmail.com>
2025-03-02 18:44:17 +0700

    create

---
+++ src/main/java/com/example/LeThiThuyNgan/LeThiThuyNganApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeThiThuyNgan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeThiThuyNganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeThiThuyNganApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeThiThuyNgan/LeThiThuyNganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeThiThuyNgan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeThiThuyNganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
